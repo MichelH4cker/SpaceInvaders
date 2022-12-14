f3228504cbcf87dabf01ef703a88035eb3606c8a
MichelH4cker <michel.hecker@usp.br>
2022-12-13 21:35:11 -0300

    feat: create GameManager class with Start, Update and Finish functions (sketch)

---
+++ SpaceInvaders/src/spaceinvaders/GameManager.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spaceinvaders;
+
+/**
+ *
+ * @author michel
+ */
+public class GameManager {
+    
+    public void Start(){
+        
+    }
+    
+    public void Update(){
+        
+    }
+    
+    public void Finish(){
+        
+    }
+    
+}
