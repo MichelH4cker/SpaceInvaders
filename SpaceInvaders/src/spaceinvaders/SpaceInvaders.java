dc3ce581682f2abfcbaef094f9ad0d2901aedcbb
MichelH4cker <michel.hecker@usp.br>
2022-12-14 08:46:57 -0300

    refactor: kill SceneBuilder and switch to canvas and graphic context

--- SpaceInvaders/src/spaceinvaders/SpaceInvaders.java
+++ SpaceInvaders/src/spaceinvaders/SpaceInvaders.java
@@ -7,8 +7,16 @@ package spaceinvaders;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -19,12 +27,34 @@ public class SpaceInvaders extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
+        stage.setTitle( "Canvas Example" );
+         
+        Group root = new Group();
+        Scene theScene = new Scene( root );
+        stage.setScene( theScene );
+
+        Canvas canvas = new Canvas( 400, 200 );
+        root.getChildren().add( canvas );
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        gc.setFill( Color.RED );
+        gc.setStroke( Color.BLACK );
+        gc.setLineWidth(2);
+        Font theFont = Font.font( "Times New Roman", FontWeight.BOLD, 48 );
+        gc.setFont( theFont );
+        gc.fillText( "Hello, World!", 60, 50 );
+        gc.strokeText( "Hello, World!", 60, 50 );
+
+
         stage.show();
+
+//        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+//        
+//        Scene scene = new Scene(root);
+//        
+//        stage.setScene(scene);
+//        stage.show();
     }
 
     /**
@@ -32,6 +62,10 @@ public class SpaceInvaders extends Application {
      */
     public static void main(String[] args) {
         launch(args);
+        
+        GameManager Game = new GameManager();
+        
+        Game.Start();
     }
     
 }
